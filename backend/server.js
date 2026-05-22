5783448f90e711293d5fb396b78f22b6e0b30a9c
kumarikashish-04 <kashish87897231@gmail.com>
2026-05-22 23:43:30 +0530

    deployed frontend and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const cors = require("cors");
+
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
@@ -10,22 +12,46 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://smart-store-rho.vercel.app",
+
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/ai", require("./routes/aiRoutes"));
 app.use(
-    "/api/analytics",
-    require("./routes/analyticsRoutes")
-  );
-const PORT = process.env.PORT || 5000;
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/products",
+  require("./routes/productRoutes")
+);
+
+app.use(
+  "/api/ai",
+  require("./routes/aiRoutes")
+);
+
+app.use(
+  "/api/analytics",
+  require("./routes/analyticsRoutes")
+);
+
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(
+    `Server running on ${PORT}`
+  );
 });
